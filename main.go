c1e7e94ecec1e3e0e5e28b4aba7859fc0c4d56be
RuanZzzz <734162396@qq.com>
2022-04-09 10:29:45 +0800

    显示文章详情内容

--- main.go
+++ main.go
@@ -78,10 +78,44 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>未找到请求页面 :(</h1>"+"<p>如果有疑惑请联系我们</p>")
 }
 
+// Article 对应一条文章数据
+type Article struct {
+	Title, Body string
+	ID          int64
+}
+
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	// 1、获取URL参数
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Fprint(w, "文章 ID："+id)
+
+	// 2、读取对应文章数据
+	article := Article{}
+	query := "SELECT * FROM articles WHERE id = ?"
+	// QueryRow读取单条数据
+	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
+
+	// 3、如果出现错误
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// 3.1 未找到数据
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 文章找不到")
+		} else {
+			// 3.2 数据库错误
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+	} else {
+		//4. 读取成功
+		tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml")
+		checkError(err)
+
+		err = tmpl.Execute(w, article)
+		checkError(err)
+	}
+
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
