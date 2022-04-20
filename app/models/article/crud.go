0b8ab9e3f90e3db9d25825c9faa9834d5170f43b
RuanZzzz <734162396@qq.com>
2022-04-20 23:48:37 +0800

    完善模型中的create方法

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -29,8 +29,7 @@ func GetAll() ([]Article, error) {
 
 // Create 创建文章，通过 article.ID 来判断是否创建成功
 func (article *Article) Create() (err error) {
-	result := model.DB.Create(&article)
-	if err = result.Error; err != nil {
+	if err = model.DB.Create(&article).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
